736b5d3af18ebc42f1cb94a39c7fa35e1995f58a
Michael Melesse <micmelesse@gmail.com>
2017-10-22 00:36:16 -0400

    removed cluter from 3d code

--- cube.js
+++ cube.js
@@ -1,19 +1,16 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+var camera = new THREE.PerspectiveCamera();
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshBasicMaterial({
   color: 0xff0000
 });
+
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-
 camera.position.z = 5;
 
+document.body.appendChild(renderer.domElement);
 function animate() {
   requestAnimationFrame(animate);
   cube.rotation.y += 0.1;
